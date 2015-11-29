afc54ee7b48079b5d292394abd8c2aa6c2e56961
Adam Venturella <aventurella@blitzagency.com>
2015-11-29 14:06:58 -0800

    Initial Commit

---
+++ Events/Events.h
@@ -0,0 +1,19 @@
+//
+//  Events.h
+//  Events
+//
+//  Created by Adam Venturella on 11/29/15.
+//  Copyright © 2015 BLITZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Events.
+FOUNDATION_EXPORT double EventsVersionNumber;
+
+//! Project version string for Events.
+FOUNDATION_EXPORT const unsigned char EventsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Events/PublicHeader.h>
+
+
